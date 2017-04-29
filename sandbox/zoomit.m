6d1da3b822c583dbe22a5b12cff307f30927ddd6
davecan <empty>
2017-04-29 17:44:29 -0500

    updated for MATLAB

--- sandbox/zoomit.m
+++ sandbox/zoomit.m
@@ -1,10 +1,16 @@
-# zooms image to arbitrary size c > 1
-function y = zoomit(x, c=2)
-  y = zeros(c * rows(x), c * columns(x));
-  for m = 1:c
-    for n = 1:c
-      y(m:c:end, n:c:end) = x;
+% basic zoom algorithm
+function s = zoomit(img, c)
+  [rows, cols] = size(img);
+  s = ones(c * rows, c * cols);
+  for m = 1:rows
+    for n = 1:cols
+      p = img(m,n);
+      s(m+m-1,n+n-1) = p;
+      s(m+m-1,n+n) = p;
+      s(m+m,n+n-1) = p;
+      s(m+m,n+n) = p;
     end
   end
+  s = uint8(s);
 end
   
